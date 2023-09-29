507c3f6c8cae0c528dd9d3ba391de9b91b0740e5
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-09-29 09:04:47 -0300

    Create index2909.php

---
+++ index2909.php
@@ -0,0 +1,25 @@
+<?php
+$pessoa1 = "Pedro";
+$pessoa2 = "Robson";
+
+if(5 > 2){
+    echo "verdade";
+} else {
+    echo "Falso";
+}
+echo "<br>";
+
+if($pessoa1 == $pessoa2){
+    echo "Verdade";
+} else {
+    echo "Falso";
+}
+echo "<br>";
+
+if(12 <= 11) {
+    echo "Verdadeiro";
+} else {
+    echo "Falso";
+}
+echo "<br>";
+?>
