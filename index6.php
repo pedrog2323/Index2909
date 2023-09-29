d8f56cc0c77fba8f3c69cc4fd54024b414269047
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-09-29 14:07:46 -0300

    kjhgf

---
+++ index6.php
@@ -0,0 +1,13 @@
+<?php
+   $algo = array(10,20,30,40,50,60,70,80,90,100);
+
+   foreach($algo as $ei){
+        if($ei == 30 || $ei == 40){
+            continue;
+            
+   }
+   echo $ei +0;
+   echo "<br>";
+}
+
+?>
