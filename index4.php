694b95a060125cb5d82bcb3cb4fa8cec1662826b
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-09-29 11:45:32 -0300

    Create index4.php

---
+++ index4.php
@@ -0,0 +1,14 @@
+<?php
+    $v1 = 40;
+
+    if($v1 >= 41 && $v1 <= 50){
+        echo "Velocidade correta";
+    } else if($v1 == 40){
+        echo "Atenção";
+    } else if($v1 = 50){
+        echo "Multadoo!!!";
+    } else {
+        echo"nada";
+    }
+
+?>
